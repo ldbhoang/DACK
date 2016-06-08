e4c315588de23063647068eeffaa8f07dd25f3e7
ldbhoang <ldbhoang@yahoo.com.vn>
2016-06-08 07:00:33 +0700

    Update admin, cssTuts, jsTuts pages Update edit/delete post

--- javascript/app.js
+++ javascript/app.js
@@ -5,9 +5,12 @@ var myApp = angular.module('mainApp', [
 	'mainApp.home',
 	'mainApp.login',
 	'mainApp.html',
+	'mainApp.css',
+	'mainApp.js',
 	'mainApp.addPost',
-	'mainApp.discuss',
-	'mainApp.admin'
+	'mainApp.admin',
+	'mainApp.editPost'
+	'mainApp.discuss'
 ]);
 
 myApp.config(['$routeProvider', function($routeProvider){
