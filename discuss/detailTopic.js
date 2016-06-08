de95951080c858b3e46d2e85b1f6a72657abb53c
ldbhoang <ldbhoang@yahoo.com.vn>
2016-06-08 15:00:42 +0700

    Fix some errors on discuss and topicDetail page

--- discuss/detailTopic.js
+++ discuss/detailTopic.js
@@ -14,45 +14,68 @@ detailTopicApp.config(['$routeProvider', function($routeProvider){
 }]);
 
 detailTopicApp.controller('detailTopicCtrl', ['$scope', '$firebase', '$firebaseArray', '$location', '$route', 'UserService', function($scope, $firebase, $firebaseArray, $location, $route, UserService) {
-    var fb = new Firebase("https://frontend-tuts.firebaseio.com/discuss");
-    $scope.Topics = $firebaseArray(fb);
-    var idTopic = $route.current.params.id;
-    $scope.Topics.$loaded().then(function () {
-        console.log(idTopic);
-        var i;
-            for (i = 0; i < $scope.Topics.length; i++) {
-                if (idTopic === $scope.Topics[i].$id) {
-                    $scope.detailTopic = $scope.Topics[i];
-                    break;
-                }
-            }
-		if($scope.detailTopic == null)
+	$scope.service = UserService;
+	var loggedIn = false;
+    $scope.$watch('service.getUser()', function(newVal) {
+        if(!newVal || newVal=='' || newVal == 'failed') {
+            loggedIn = false;
+        }
+		else
 		{
-			$location.path('/discuss');
+			loggedIn = true;
 		}
-    });
+		
+		var fb = new Firebase("https://frontend-tuts.firebaseio.com/discuss");
+		$scope.Topics = $firebaseArray(fb);
+		var idTopic = $route.current.params.id;
+		$scope.Topics.$loaded().then(function () {
+			console.log(idTopic);
+			var i;
+				for (i = 0; i < $scope.Topics.length; i++) {
+					if (idTopic === $scope.Topics[i].$id) {
+						$scope.detailTopic = $scope.Topics[i];
+						break;
+					}
+				}
+			if($scope.detailTopic == null)
+			{
+				console.log("balba");
+				$location.path('/discuss');
+			}
+		});
+		console.log("balba");
+		$scope.Reply = function(){
+			if(loggedIn)
+			{
+				console.log("balba");
+				var content = $scope.reply.Content;
+				var user = UserService.getUser();
+				console.log(content);
+				var fb = new Firebase("https://frontend-tuts.firebaseio.com/discuss/" + idTopic);
+				var ans = fb.child('answers');
+				var d = new Date();
+				ans.push({
+					content: content,
+					emailreply: user,
+					datePublish: d.toLocaleTimeString() + ' ' +  d.toDateString()
+				},function(error) {
+					if (error) {
+						console.log(error);
+					}
+					else {
+						$('#replyModal').modal('hide');
+						console.log("Push data success");
+						$route.reload();
+					}
+				});
+			}
+			else
+			{
+				alert("You must sign in to use this feature!");
+				$('#replyModal').modal('hide');
+				$route.reload();
+			}
 
-    $scope.Reply = function(){
-        var content = $scope.reply.Content;
-        var user = UserService.getUser();
-        console.log(content);
-        var fb = new Firebase("https://frontend-tuts.firebaseio.com/discuss/" + idTopic);
-        var ans = fb.child('answers');
-        var d = new Date();
-        ans.push({
-            content: content,
-            emailreply: user,
-            datePublish: d.toLocaleTimeString() + ' ' +  d.toDateString()
-        },function(error) {
-            if (error) {
-                console.log(error);
-            }
-            else {
-                $('#replyModal').modal('hide');
-                console.log("Push data success");
-
-            }
-        });
-
-    }
+		}
+	});		
 }]);
--- discuss/discussScript.js
+++ discuss/discussScript.js
@@ -13,40 +13,62 @@ discussApp.config(['$routeProvider', function($routeProvider){
     })
 }]);
 
-discussApp.controller('discussCtrl', ['$scope','$location','UserService' ,'$firebase', '$firebaseArray',function($scope,$location,UserService,$firebase, $firebaseArray){
+discussApp.controller('discussCtrl', ['$scope','$location','UserService' ,'$firebase', '$firebaseArray', '$route', function($scope,$location,UserService,$firebase, $firebaseArray, $route){
     $scope.service = UserService;
+	var loggedIn = false;
     $scope.$watch('service.getUser()', function(newVal) {
         if(!newVal || newVal=='' || newVal == 'failed') {
-            $location.path('/login');
+            loggedIn = false;
         }
+		else
+		{
+			loggedIn = true;
+		}
         var firebaseObj = new Firebase("https://frontend-tuts.firebaseio.com/discuss/");
 
         $scope.Topics = $firebaseArray(firebaseObj);
 
         $scope.createTopic = function(){
-            var topic = $scope.newTopic.Topic;
-            var content = $scope.newTopic.Content;
-            var type = $scope.newTopic.Type;
-            var user = UserService.getUser();
-            var d = new Date();
-            var fb = new Firebase("https://frontend-tuts.firebaseio.com/discuss/");
-
-            fb.push({
-                topic: topic,
-                content: content,
-                type: type,
-                emailOwner: user,
-                datePublish: d.toLocaleTimeString() + ' ' +  d.toDateString()
-            },function(error) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    $('#newTopicModal').modal('hide');
-                    console.log("Push data success");
-
-                }
-            });
+			console.log(loggedIn);
+			if(loggedIn)
+			{
+				var topic = $scope.newTopic.Topic;
+				var content = $scope.newTopic.Content;
+				var type = $scope.newTopic.Type;
+				var user = UserService.getUser();
+				var d = new Date();
+				var fb = new Firebase("https://frontend-tuts.firebaseio.com/discuss/");
+				
+				if(type == null || type === "")
+				{
+					alert("Please select a category!!!");
+				}
+				else
+				{
+					fb.push({
+					topic: topic,
+					content: content,
+					type: type,
+					emailOwner: user,
+					datePublish: d.toLocaleTimeString() + ' ' +  d.toDateString()
+					},function(error) {
+						if (error) {
+							console.log(error);
+						}
+						else {
+							$('#newTopicModal').modal('hide');
+							console.log("Push data success");
+							$route.reload();
+						}
+					});
+				}				
+			}
+            else
+			{
+				alert("You must sign in to use this feature!");
+				$('#newTopicModal').modal('hide');
+				$route.reload();
+			}
         };
     });
 
